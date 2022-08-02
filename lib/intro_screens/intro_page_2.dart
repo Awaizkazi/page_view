55bb8170818fbf734a3addf40d81a60b1a537cb2
Awaizkazi <88947135+Awaizkazi@users.noreply.github.com>
2022-08-02 21:06:11 +0530

    c

--- lib/intro_screens/intro_page_2.dart
+++ lib/intro_screens/intro_page_2.dart
@@ -7,6 +7,8 @@ class IntroPage2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
+      child: Image.network(
+          'https://thumbs.dreamstime.com/b/fast-delivery-service-courier-bike-flat-design-illustration-vector-wearing-protective-mask-concept-safe-lock-down-208237623.jpg'),
     );
   }
 }
