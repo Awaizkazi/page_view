520551887033ad6a987ae4d1b391c475a34a7c95
Awaizkazi <88947135+Awaizkazi@users.noreply.github.com>
2022-08-02 22:55:17 +0530

    c

--- lib/intro_screens/iintro_page_3.dart
+++ lib/intro_screens/iintro_page_3.dart
@@ -7,8 +7,7 @@ class IntroPage3 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Image.network(
-          'https://previews.123rf.com/images/juliatim/juliatim1406/juliatim140600059/29651017-delivery-service-man-with-a-box-in-his-hands.jpg'),
+      child: Image.asset('assets/Foodorder.png'),
     );
   }
 }
--- lib/intro_screens/intro_page_2.dart
+++ lib/intro_screens/intro_page_2.dart
@@ -7,8 +7,7 @@ class IntroPage2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Image.network(
-          'https://thumbs.dreamstime.com/b/fast-delivery-service-courier-bike-flat-design-illustration-vector-wearing-protective-mask-concept-safe-lock-down-208237623.jpg'),
+      child: Image.asset('assets/FastDelivery.png'),
     );
   }
 }
